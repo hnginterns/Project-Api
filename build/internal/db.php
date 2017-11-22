ccbddeaee8a89c56ed4f7a296444772ad8b7120b
Seyi Onifade <xyluz@ymail.com>
2017-11-22 02:07:26 +0100

    db connection fix

--- build/internal/db.php
+++ build/internal/db.php
@@ -1,7 +1,7 @@
 <?php
 /* Database connection settings */
-$host = 'localhost';
+$host = '174.30.1.4';
 $user = 'root';
-$pass = '';
+$pass = 'I03!7v]KxS';
 $db = 'test';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
